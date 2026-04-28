2f6b78dd529b4e3dda9606dab98a7e096c50f822
lorranymag <levangelist@my.smccd.edu>
2026-04-28 09:42:13 -0700

    Edits to README

--- README.md
+++ README.md
@@ -1,18 +1,15 @@
-## Getting Started
+# Final Project 
 
-Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
+Welcome to my Getting Started with GUI application.
 
-## Folder Structure
+## Author: Lorrany Evangelista Schiavo Magalhaes 
 
 The workspace contains two folders by default, where:
 
 - `src`: the folder to maintain sources
-- `lib`: the folder to maintain dependencies
+- `doc`: the folder to maintain documentation related to my project (UML diagram, etc)
 
-Meanwhile, the compiled output files will be generated in the `bin` folder by default.
+This projects teaches the basics of Swing 
 
-> If you want to customize the folder structure, open `.vscode/settings.json` and update the related settings there.
-
-## Dependency Management
-
-The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
+## How to use the application: 
+### Basic Functionality
--- src/ToDoList.java
+++ src/ToDoList.java
@@ -20,20 +20,20 @@ public class ToDoList {
         int choice = input.nextInt();
         input.nextLine(); 
 
-        if (choice == 1) {
+    if (choice == 1) {
             System.out.println("Enter your task: ");
             String task = input.nextLine();
             tasks.add(task);
             completed.add(false);
             }
-        else if (choice == 2) {
+    else if (choice == 2) {
             System.out.println("Your tasks:");
             for (int i = 0; i < tasks.size(); i++) {
             String status = completed.get(i) ? "[✔]" : "[ ]";
             System.out.println((i + 1) + ". " + status + " " + tasks.get(i));
                 }   
             }
-        else if (choice == 3) {
+    else if (choice == 3) {
             System.out.println("Enter the number of the task you completed: ");
             int taskNumber = input.nextInt();
             if (taskNumber > 0 && taskNumber <= tasks.size()) {
@@ -41,7 +41,7 @@ public class ToDoList {
                 System.out.println("Task marked as completed.");
                 }
             }
-        else if (choice == 4) {
+    else if (choice == 4) {
             System.out.println("Exiting the To-Do List. Goodbye!");
             break;
             }
